4c736d2201e4b523ea3eda93852b7e15a91f2bb4
Franco <andriamitantsoa36kofra@gmail.com>
2024-02-14 11:36:26 +0300

    add navigation bottom menu

---
+++ lib/utils/navigation_menu.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+            elevation: 0,
+            selectedIndex: controller.selectedIndex.value,
+            onDestinationSelected: (index) =>
+                controller.selectedIndex.value = index,
+            destinations: const [
+              NavigationDestination(icon: Icon(Iconsax.home), label: 'Accueil'),
+              NavigationDestination(
+                  icon: Icon(Iconsax.calendar_2), label: 'Sessions'),
+              NavigationDestination(
+                  icon: Icon(Iconsax.chart_3), label: 'Stats'),
+              NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+            ]),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+  final screens = [
+    Container(
+      color: Colors.yellow,
+    ),
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.green,
+    ),
+  ];
+}
