a5fbe8217cb8c0644a5a9d412566411e32b47a30
Franco <andriamitantsoa36kofra@gmail.com>
2024-02-14 16:37:08 +0300

    eitd navigation menu,nt use GetX

---
+++ lib/services/session_service.dart
@@ -0,0 +1,39 @@
+import 'package:http/http.dart' as http;
+
+class Session {
+  final DateTime start;
+  final DateTime end;
+  final int buyin;
+  final int buyout;
+  final String place;
+
+  const Session({
+    required this.start,
+    required this.end,
+    required this.buyin,
+    required this.buyout,
+    required this.place,
+  });
+
+  factory Session.fromJson(Map<String, dynamic> json) {
+    return Session(
+        start: json['start'],
+        end: json['end'],
+        buyin: json['buyin'],
+        buyout: json['buyout'],
+        place: json['place']);
+  }
+}
+
+class SessionService {
+  Future<List<Session>> getSession() async {
+    final response = await http.get(
+      Uri.parse("http://localhost:3001/api/session"),
+      headers: {
+        'Content-Type': 'application-json',
+        'Authorization': "",
+      },
+    );
+    print(response.statusCode);
+  }
+}
